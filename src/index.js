b78ab0f60de8cea7f81ea6242dcb00beb8e8000e
janChorny <janchorny@tut.by>
2022-02-24 19:37:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+	let bracketsConfigToString = ''
+  for (let i = 0; i < bracketsConfig.length; i++){
+	  bracketsConfigToString = bracketsConfig[i].join('');
+	  if (str.includes(bracketsConfigToString)){
+		  str = str.replace(bracketsConfigToString, '');
+		  i = -1
+	  }
+  }
+   return !str;
 }
